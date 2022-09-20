af24c0ce665407be9a5d5c17825344159ba46193
Emily  Morgan <111945828+emilynm2001@users.noreply.github.com>
2022-09-19 23:19:52 -0500

    Added Icons and styling to Navigation; revised location in state

--- public/Components/Navigation/index.js
+++ public/Components/Navigation/index.js
@@ -1,25 +1,80 @@
 import React from "react";
-import { View, Button } from "react-native";
+import { 
+    SafeAreaView,
+    StyleSheet,
+    View,
+    Text,
+    Image,
+    Button,
+    TouchableOpacity,
+  } from "react-native";
+const a = require('./Images/bell.png')
+const b = require('./Images/clipboard-list.png')
+const c = require('./Images/comment.png')
+const d=require('./Images/calendar-lines.png')
+const e=require('./Images/menu-dots.png')
 
 export default class Navigation extends React.Component {
     constructor() {
         super();
         this.state = {
-            VIEW_NAMES: ['CLASS', 'ASSIGNMENT', 'NOTIFICATIONS', 'CALENDAR', 'SETTING']
+            //VIEW_NAMES: ['CLASS', 'ASSIGNMENT', 'NOTIFICATIONS', 'CALENDAR', 'SETTING']
+            VIEW_NAMES: [['NOTIFICATIONS', a],['ASSIGNMENT', b],
+             ['CHAT', c],['CALENDAR', d],['SETTING', e]]
         }
     }
     
     render() {
         return (
-            <View>
-                {this.state.VIEW_NAMES.map((NAME, index) => (
-                    <Button 
-                        key={index}
-                        title={'View ' + NAME}
-                        onPress={() => {this.props.UPDATE_VIEW('VIEW_' + NAME)}}
+            <View style={styles.buttonRowStyle}>
+                {this.state.VIEW_NAMES.map(([NAME, IMG], index) => (
+                    <TouchableOpacity
+                    style={styles.buttonRowStyle}
+                    activeOpacity={0.5}
+                    onPress={() => {this.props.UPDATE_VIEW('VIEW_' + NAME)}}
+                    key={index}>
+                    <Image
+                      source={IMG}
+                      style={styles.buttonImageIconStyle}
                     />
+                    <Text style={styles.buttonTitleStyle}>{ '\n' + NAME}</Text>
+                    <View style={styles.buttonIconSeparatorStyle} />
+                  </TouchableOpacity>
                 ))}
             </View>
         )
     }
 }
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      margin: 10,
+      marginTop: 30,
+      padding: 30,
+    },
+    buttonRowStyle: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      backgroundColor: '#9E5DFC',
+      height: 40,
+      margin: 5,
+    },
+    buttonImageIconStyle: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      padding: 10,
+      margin: 5,
+      height: 25,
+      width: 25,
+      resizeMode: 'stretch',
+    },
+    buttonTitleStyle:{
+     textAlign: 'center',
+     display: 'block'
+    },
+    buttonIconSeparatorStyle: {
+      backgroundColor: '#fff',
+      width: 1,
+      height: 40,
+    },
+});
