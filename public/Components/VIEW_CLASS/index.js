0db45c43c47c9f1ec2e7980343f0215507261b10
Emily  Morgan <111945828+emilynm2001@users.noreply.github.com>
2022-11-02 21:08:38 -0500

    recommiting class color change

--- public/Components/VIEW_CLASS/index.js
+++ public/Components/VIEW_CLASS/index.js
@@ -13,78 +13,67 @@ import { ADD_CLASS_ACTION } from "../../Actions/List_Action";
 import { CLASS_OBJECT_CREATION } from "../../Util/Object_Creation";
 import reactDom, { render } from "react-dom";
 import { TouchableWithoutFeedback } from "react-native-web";
+import { Class_Style, randomColor } from "../../Util/style";
 
 // CLASS_OBJECT_CREATION(class_ID, name, desc, professor_ID)
 
 class VIEW_CLASS extends React.Component {
   constructor() {
     super();
-  }
-
+      this.state = {
+        colors: ["#CDF5D8","#FFE18A", "#C1FF8A", "#FFB7A0", "#8AD8FF", "#F2B3F9","#8AFFE6"]
+      }
+    }
+  
+  
   // This component should not load ALL CRNS/Possible classes
   // Should have child class which when activated, loads the bunch
   // Else, this view is just for the user's ALREADY ADDED classes!
 
   render() {
     return (
-
-      <View style={styles.container}>
+      <View style={Class_Style.container}>
         {/* Mapping through classes in user profile already as of 9/17, pulling from array in global state */}
         {/*<Text style={{ fontSize: 20 }}>
           You have {this.props.CLASS_STATE.CLASSES.length} classes.
     </Text>*/}
-        <ScrollView>
-          {this.props.CLASS_STATE.CLASSES.length > 0 ? (
-            this.props.CLASS_STATE.CLASSES.map((CLASS, index) => (
-              /*
-              <TouchableHighlight
-                key={index}
-                activeOpacity={0.6}
-                underlayColor="#DDDDDD"
-                onPress={() => alert(CLASS.class_ID)}
-              >*/
-                <>
-                  {index == 0 ? (
-                    <>
-                      <View style={[styles.classContainerFirst, styles.shawdowProp]}>
-                        <Text style={styles.classTitleTx} key={index}>
-                          {CLASS.name} -{"\n"}
-                        </Text>
-                        <Text style={styles.classMainTxt}>
-                          {CLASS.desc}
-                          {"\n"}
-                          {/* Ideally, when we implement the db, we can pull the professor's info for class based on foreign key */}
-                          {CLASS.date}
-                        </Text>
-                      </View>
-                      <Text> {"\n"}</Text>
-                    </>
-                  ) : (
-                    <>
-                      <View style={styles.classContainerSecond}>
-                        <Text style={styles.classTitleTx2} key={index}>
-                          {CLASS.name} -{"\n"}
-                        </Text>
-                        <Text style={styles.classMainTxt2}>
-                          {CLASS.desc}
-                          {"\n"}
-                          {/* Ideally, when we implement the db, we can pull the professor's info for class based on foreign key */}
-                          {CLASS.date}
-                        </Text>
-                      </View>
-                      <Text> {"\n"}</Text>
-                    </>
-                  )}
-                </>
-              /*</TouchableHighlight>*/
-            ))
-          
-          ) : (
-            <Text style={{ fontSize: 20 }}>
-              Look's like you have no classes!
-            </Text>
-          )}
-        </ScrollView>
+          <ScrollView>
+            {this.props.CLASS_STATE.CLASSES.length > 0 ? (
+              this.props.CLASS_STATE.CLASSES.map((CLASS, index) => (
+                <TouchableHighlight
+                  key={index}
+                  activeOpacity={0.6}
+                  underlayColor="#DDDDDD"
+                  onPress={() => alert(CLASS.class_ID)}
+                >
+                  <>
+              
+                      <>
+                        <View style={[Class_Style.classContainer, {backgroundColor: this.state.colors[index]}, Class_Style.shawdowProp]}>
+                          <Text style={Class_Style.classTitleTx} key={index}>
+                            {CLASS.name}:{"\n"}
+                          </Text>
+                          <Text style={Class_Style.classMainTxt}>
+                            {CLASS.desc}
+                            {/* Ideally, when we implement the db, we can pull the professor's info for class based on foreign key */}
+                            
+                          </Text>
+                          <Text style={Class_Style.dateTxt}>
+                            {CLASS.date}
+                          </Text>
+                        </View>
+                        <Text> {"\n"}</Text>
+                      </>
+                  </>
+                </TouchableHighlight>
+              ))
+            ) : (
+              <Text style={{ fontSize: 20 }}>
+                Look's like you have no classes!
+              </Text>
+            )}
+            <View style={{height: 80}}></View>
+          </ScrollView>
         {/* ADJUST BUTTON ONCLICK TO BRING LIST OF CLASSES BY PROFESSOR_ID -- USER SHOULD ONLY BE ABLE TO ADD CLASSES BASED ON WHAT PROFESSOR THEY BELONG TO */}
         {/*<Button
           title="Add Class"
@@ -118,78 +107,3 @@ const mapDispatchToProps = (dispatch) =>
   );
 
 export default connect(mapStateToProps, mapDispatchToProps)(VIEW_CLASS);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 10,
-    alignItems: "center",
-  },
-  classContainerFirst: {
-    height: 139,
-    width: 360,
-    backgroundColor: "#FFA386",
-    borderRadius: 30,
-    //overflow: "hidden",
-    justifyContent: "center",
-    //borderWidth: 1,
-  },
-  shawdowProp:{
-    shadowColor:"#171717",
-    shadowOffset:{width: -2, height: 4},
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
-  },
-  classContainerSecond: {
-      height: 133,
-      width: 259,
-      backgroundColor: "#CDF5D8",
-      borderRadius: 30,
-      overflow: "hidden",
-      justifyContent: "center",
-     // borderWidth: 1,
-      marginLeft: 100,
-      marginTop: -10,
-  },
-  classTitleTx: {
-    textAlign: "left",
-    textAlignVertical: "top",
-    paddingVertical: 20,
-    paddingHorizontal: 20,
-    fontWeight: "bold",
-    color: "white",
-    fontSize: 20,
-    marginVertical: -30,
-    //flexGrow: 1,
-  },
-  classTitleTx2: {
-    textAlign: "left",
-    textAlignVertical: "top",
-    paddingVertical: 20,
-    paddingHorizontal: 20,
-    fontWeight: "bold",
-    color: "black",
-    fontSize: 20,
-    marginVertical: -30,
-    //flexGrow: 1,
-  },
-  classMainTxt: {
-    height: 51,
-    width: "100%",
-    // backgroundColor:"blue",
-    fontSize: 14,
-    color: "white",
-    textAlign: "center",
-  },
-  classMainTxt2: {
-    height: 51,
-    width: "100%",
-    // backgroundColor:"blue",
-    fontSize: 14,
-    color: "black",
-    textAlign: "center",
-  },
-  classTxt: {
-    textAlign: "center",
-  },
-});
---
+++ public/Util/style.js
@@ -0,0 +1,81 @@
+export const nav = {
+  container: {
+    flex: 1,
+    margin: 10,
+    marginTop: 30,
+    padding: 30,
+    backgroundColor: "transparent"
+  },
+  buttonRowStyle: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#9E5DFC",
+    height: 40,
+    margin: 5,
+  },
+  buttonImageIconStyle: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+    margin: 5,
+    height: 25,
+    width: 25,
+    resizeMode: "stretch",
+  },
+  buttonTitleStyle: {
+    textAlign: "center",
+    // display: 'block'
+  },
+  buttonIconSeparatorStyle: {
+    backgroundColor: "#fff",
+    width: 1,
+    height: 40,
+  },
+};
+export const Class_Style = {
+  container: {
+    flex: 1,
+    marginHorizontal: 10,
+    alignItems: "center",
+  },
+  classContainer: {
+    height: 139,
+    width: 360,
+    borderRadius: 30,
+    //overflow: "hidden",
+    justifyContent: "center",
+    //borderWidth: 1,
+  },
+  shawdowProp: {
+    shadowColor: "#171717",
+    shadowOffset: { width: -2, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  classTitleTx: {
+    textAlign: "left",
+    textAlignVertical: "top",
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    fontWeight: "bold",
+    color: "black",
+    fontSize: 20,
+    marginVertical: -30,
+    //flexGrow: 1,
+  },
+  classMainTxt: {
+    height: 51,
+    width: "100%",
+    //backgroundColor:"blue",
+    fontSize: 14,
+    color: "black",
+    textAlign: "center",
+    paddingVertical: 15
+  },
+  classTxt: {
+    textAlign: "center",
+  },
+  dateTxt: {
+    textAlign: "center",
+  }
+}
